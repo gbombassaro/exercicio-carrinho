f0ae14cb551e4489bec0e05ea2f0e1da22d9096f
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2020-10-23 09:37:47 -0300

    Adiciona imagens e posiciona topbar

--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import {withStyles} from '@material-ui/core/styles';
-import {Box} from '@material-ui/core';
+import {Box, Grid} from '@material-ui/core';
+import {ShoppingCart} from '@material-ui/icons'
 
 const styles = (theme) => ({
   container: {
-    width: '100%',
+    width: 'calc(100% - 48px)',
     height: 70,
-    backgroundColor: theme.palette.secondary.main
+    backgroundColor: theme.palette.secondary.main,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingRight: 24,
+    paddingLeft: 24
+  },
+  brandContainer: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  brand: {
+    width: 56,
+    heigth: 56
+  },
+  icon: {
+    width: 40,
+    height: 40
   }
 })
 
 const Topbar = ({classes}) => {
   return (
-    <Box className={classes.container}>Topbar</Box>
+    <Grid className={classes.container}>
+      <Grid className={classes.brandContainer}>
+        <img src='/bulbasaur.png' className={classes.brand} />
+      </Grid>
+      <Grid>
+        <ShoppingCart className={classes.icon} />
+      </Grid>
+    </Grid>
   )
 }
 
