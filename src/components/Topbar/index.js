0caaa2f727cf81486653aa6d1daf5dad4ea0743d
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2020-10-26 21:22:16 -0300

    Var não utilizada

--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Link, useHistory} from 'react-router-dom';
 import {Grid} from '@material-ui/core';
 import {withStyles} from '@material-ui/core/styles';
