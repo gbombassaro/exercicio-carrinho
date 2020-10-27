11e58092e31e38c42ff67784257cc959a5513dab
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2020-10-26 21:55:52 -0300

    Ajusta estilos do dialog

--- src/components/Dialog/index.js
+++ src/components/Dialog/index.js
@@ -2,8 +2,34 @@ import React from 'react';
 import {Button, Dialog as MuiDialog, Typography} from '@material-ui/core'
 import {CheckCircle} from '@material-ui/icons'
 import {useHistory} from 'react-router-dom';
+import {withStyles} from '@material-ui/core/styles'
 
-const Dialog = ({open, onClose}) => {
+const styles = theme => ({
+  content: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 'calc(100% - 32px)',
+    paddingTop: theme.spacing(5),
+    paddingBottom: theme.spacing(5),
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+    [theme.breakpoints.up('md')]: {
+      minWidth: 500,
+    }
+  },
+  text: {
+    margin: theme.spacing(2, 0, 2, 0)
+  },
+  icon: {
+    width: 50,
+    height: 50,
+    fill: theme.palette.secondary.main
+  }
+})
+
+const Dialog = ({classes, open, onClose}) => {
 
   const history = useHistory();
 
@@ -15,13 +41,13 @@ const Dialog = ({open, onClose}) => {
 
   return (
     <MuiDialog open={open} onClose={onClose} disableBackdropClick disableEscapeKeyDown>
-      <div>
-        <CheckCircle/>
-        <Typography>Pedido realizado com sucesso!</Typography>
-        <Button onClick={handleClick}>Fechar</Button>
+      <div className={classes.content}>
+        <CheckCircle className={classes.icon}/>
+        <Typography className={classes.text}>Pedido realizado com sucesso!</Typography>
+        <Button variant='contained' color='secondary' onClick={handleClick}>Fechar</Button>
       </div>
     </MuiDialog>
   )
 }
 
-export default Dialog;
+export default withStyles(styles)(Dialog);
